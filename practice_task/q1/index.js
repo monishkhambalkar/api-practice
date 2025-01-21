db0b3c8b9229ff208dfd050379be55b5d7884e0a
Monish Khambalkar <monishkhambalkar@gmail.com>
2025-01-21 21:04:22 +0530

    add crud

--- practice_task/q1/index.js
+++ practice_task/q1/index.js
@@ -10,6 +10,7 @@ const users = [
   { id: 2, name: "Jane Smith", email: "jane.smith@example.com" },
 ];
 
+// 1. Create a simple GET endpoint to retrieve user information.
 app.get("/users", (req, res) => {
   res.json({
     success: true,
@@ -19,6 +20,7 @@ app.get("/users", (req, res) => {
 });
 
 // add new user
+// 2.Design a POST endpoint to add a new user.
 app.post("/users", (req, res) => {
   const { id, name, email } = req.body;
   if (!id || !name || !email) {
@@ -42,6 +44,58 @@ app.post("/users", (req, res) => {
   return res.status(201).json({
     success: true,
     message: `user is added successfully with id ${id}`,
+  });
+});
+
+// 3. Write a PUT request to update user details.
+app.put("/users/:id", async (req, res) => {
+  const userID = parseInt(req.params.id);
+  const { id, name, email } = req.body;
+
+  const userIndex = users.findIndex((user) => user.id === userID);
+  if (userIndex === -1) {
+    return res
+      .status(404)
+      .json({ success: false, message: `User With id ${userID} not found` });
+  }
+
+  try {
+    users[userIndex] = {
+      id: userID,
+      name: name || users[userIndex].name,
+      email: email || users[userIndex].email,
+    };
+    res.status(200).json({
+      success: true,
+      message: "User details updated successfully",
+      data: users[userIndex],
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error updating user Details",
+      error: error.message,
+    });
+  }
+});
+
+//4. Implement a DELETE request to remove a user.
+app.delete("/users/:id", async (req, res) => {
+  const userID = parseInt(req.params.id);
+
+  const userIndex = users.findIndex((user) => user.id === userID);
+
+  if (userIndex === -1) {
+    return res
+      .status(404)
+      .json({ success: false, message: `User with ID ${userID} not found.` });
+  }
+  const deleteUser = users.splice(userIndex, 1);
+
+  res.status(200).json({
+    success: true,
+    message: `User with ID ${userID} has been deleted.`,
+    data: deleteUser[0],
   });
 });
 
--- practice_task/q1/question.js
+++ practice_task/q1/question.js
@@ -1,3 +1,7 @@
 // 1. Create a simple GET endpoint to retrieve user information.
 
 // 2.Design a POST endpoint to add a new user.
+
+// 3. Write a PUT request to update user details.
+
+//4. Implement a DELETE request to remove a user.
